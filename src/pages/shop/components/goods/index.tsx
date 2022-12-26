b5377632e895d4f4b2fa800dcd7f179a819ac881
Xi-Yuer <2214380963@qq.com>
2022-12-26 20:55:07 +0800

    fix:路径

--- src/pages/shop/components/goods/index.tsx
+++ src/pages/shop/components/goods/index.tsx
@@ -1,14 +1,20 @@
-import { View } from '@tarojs/components'
+import { Image, View } from '@tarojs/components'
 import { FC, memo, PropsWithChildren } from 'react'
-
+import { IGoods } from 'src/service/shop/type'
+import styles from './index.module.scss'
 
 interface IProps extends PropsWithChildren {
-  data: any
+  data: IGoods
 }
 const Goods: FC<IProps> = memo((props: IProps) => {
   const { data } = props
   return (
-    <View>{data.product_name}</View>
+    <View className={styles.wrapper}>
+      <View className={styles.left}>
+        <Image src={data.picture} />
+      </View>
+      <View className={styles.rght}></View>
+    </View>
   )
 })
 
--- src/service/shop/type.ts
+++ src/service/shop/type.ts
@@ -11,9 +11,25 @@ export interface IData {
   message: string;
   status: string | number;
 }
-
+export interface IGoods {
+  create_time: string;
+  description: string;
+  id: string;
+  old_price: string;
+  picture: string;
+  price: string;
+  product_address: string;
+  product_name: string;
+  publice_status: string | number;
+  sale_count: string | number;
+  stock: string | number;
+  stock_unit: string;
+  type: string | number;
+  update_time: string;
+  userId: string | number;
+}
 export interface IGoodsData {
-  data: any[];
+  data: IGoods[];
   message: string;
   status: string | number;
   count: number;
