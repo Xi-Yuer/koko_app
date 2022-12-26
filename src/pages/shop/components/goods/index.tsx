2c898bfbe7ec634e50611261faa34b10ac22d678
Xi-Yuer <2214380963@qq.com>
2022-12-26 21:47:30 +0800

    feat:商品列表

--- src/pages/shop/components/goods/index.tsx
+++ src/pages/shop/components/goods/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View } from '@tarojs/components'
+import { Image, Text, View } from '@tarojs/components'
 import { FC, memo, PropsWithChildren } from 'react'
 import { IGoods } from 'src/service/shop/type'
 import styles from './index.module.scss'
@@ -11,9 +11,37 @@ const Goods: FC<IProps> = memo((props: IProps) => {
   return (
     <View className={styles.wrapper}>
       <View className={styles.left}>
-        <Image src={data.picture} />
+        <Image src={data.picture} mode='aspectFill' />
       </View>
-      <View className={styles.rght}></View>
+      <View className={styles.right}>
+        <View className={styles.name}>
+          <Text>{data.product_name}</Text>
+        </View>
+        <View className={styles.description}>
+          <Text>{data.description}</Text>
+        </View>
+        <View className={styles.info}>
+          <Text>剩余:{data.stock}{data.stock_unit}</Text>
+          <Text>{data.product_address}</Text>
+        </View>
+        <View className={styles.price_info}>
+          <View className={styles.control}>
+            <View>
+              <Text className={styles.price}>
+                <Text className={styles.price_}>价格:￥</Text>
+                {data.price}
+              </Text>
+              <Text className={styles.old_price}>
+                <Text className={styles.oldPrice_}>原价:￥</Text>
+                {data.old_price}
+              </Text>
+            </View>
+            <View className={styles.buy_box}>
+              <Text >立即购买</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   )
 })
