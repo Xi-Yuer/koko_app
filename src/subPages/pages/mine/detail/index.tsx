38a708c1d0aff268e605e214b64fea4f0fb9c67b
Xi-Yuer <2214380963@qq.com>
2023-01-18 13:54:39 +0800

    feat:个人中心

--- src/subPages/pages/mine/detail/index.tsx
+++ src/subPages/pages/mine/detail/index.tsx
@@ -1,9 +1,63 @@
-import { View } from '@tarojs/components'
+import { Image, View } from '@tarojs/components'
 import { memo } from 'react'
+import { useSelector } from 'react-redux'
+import { AtIcon } from 'taro-ui'
+
+import styles from './index.module.scss'
 
 const Detail = memo(() => {
+
+  const { detail, address } = useSelector<any, any>(state => state.user)
+
+  console.log(detail)
   return (
-    <View>Detail</View>
+    <View className={styles.wrapper}>
+      <View className={styles.avatar}>
+        <View className={styles.left}>
+          <View className={styles.img}>
+            <Image src={detail.avatar} mode='aspectFill'></Image>
+          </View>
+        </View>
+        <View className={styles.flex}>
+          修改头像<AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+      <View className={styles.row_i}>
+        <View className={styles.left}>名字</View>
+        <View className={styles.center}>{detail.name}</View>
+        <View className={styles.right}>
+          <AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+      <View className={styles.row_i}>
+        <View className={styles.left}>简介</View>
+        <View className={styles.center}>{detail.asign}</View>
+        <View className={styles.right}>
+          <AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+      <View className={styles.row_i}>
+        <View className={styles.left}>性别</View>
+        <View className={styles.center}>{!detail.gender ? '女' : '男'}</View>
+        <View className={styles.right}>
+          <AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+      <View className={styles.row_i}>
+        <View className={styles.left}>权限</View>
+        <View className={styles.center}>{detail.is_admin ? '管理员' : '非管理员'}</View>
+        <View className={styles.right}>
+          <AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+      <View className={styles.row_i}>
+        <View className={styles.left}>地址</View>
+        <View className={styles.center}>{address.cityName + address.countyName + address.detailInfo}</View>
+        <View className={styles.right}>
+          <AtIcon value='chevron-right' size='20' color='grey'></AtIcon>
+        </View>
+      </View>
+    </View>
   )
 })
 
