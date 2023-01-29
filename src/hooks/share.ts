f5c5190b443eb2e5745118ebccc3771bd16f4803
Xi-Yuer <2214380963@qq.com>
2023-01-29 21:27:10 +0800

    style:share

--- src/hooks/share.ts
+++ src/hooks/share.ts
@@ -3,7 +3,7 @@ import { useShareAppMessage } from "@tarojs/taro";
 export const useSharePage = () => {
   useShareAppMessage(() => {
     return {
-      title: "小刚副业，生鲜食品的搬运工",
+      title: "小刚副业，生鲜农产品的搬运工",
       imageUrl:
         "https://koko-img-oss.oss-cn-chengdu.aliyuncs.com/static/logo_bg.jpg",
     };
